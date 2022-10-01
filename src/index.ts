89dcb0eca63a72a04c9a7db24b28cc6a5828f27d
Seb Renauld <sebastien.renauld@solveq.io>
2022-10-01 04:15:45 +0200

    feat: dead reckoning

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Decimate_DouglasPeucker } from "./douglas-peucker";
 import { DecimateBatch, DecimateOnline } from "./prelude/interfaces/decimate";
 import { Decimate_STTrace } from "./sttrace/decimate-sttrace";
 const Decimate = {
-    DeadReckoning: Decimate_DeadReckoning
+    DeadReckoning: Decimate_DeadReckoning,
     STTrace: Decimate_STTrace,
     DouglasPeucker: Decimate_DouglasPeucker
 }
